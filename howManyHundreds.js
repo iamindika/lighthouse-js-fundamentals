353617990b345a9811d85057364b795c855a808f
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-09-12 17:57:19 +0000

    How many containers of hundred bottles of maple syrup to send to mars

---
+++ howManyHundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds (bottles) {
+  let bottlesToKeep = bottles % 100;
+  let boxesToShip = (bottles - bottlesToKeep) / 100;
+  return boxesToShip;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
