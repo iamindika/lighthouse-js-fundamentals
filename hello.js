a1e0623d5555eb599482103a7c492bbf05c7eacf
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-09-12 05:22:33 +0000

    How computers say Hello

---
+++ hello.js
@@ -0,0 +1,18 @@
+/* Say Hello
+
+const sayHello = function () {
+	console.log("Hello, world");
+};
+
+sayHello();
+*/
+
+// Say Hello version 2.0
+
+const sayHello = function (name) {
+	console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
