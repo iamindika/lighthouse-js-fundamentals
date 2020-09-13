1002295823262a7f8506e08672ceef0851d74b06
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-09-13 05:30:07 +0000

    Concatenating two arrays

---
+++ concat.js
@@ -0,0 +1,12 @@
+function concat (pollData1, pollData2) {
+  let i = pollData2.length;
+  while (i > 0) {
+    pollData1.push(pollData2.pop());
+  }
+  return pollData1;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
