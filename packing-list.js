e2d7c7012217e828c867eb1b30a6c140cf6abedf
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-09-13 03:19:22 +0000

    Using a for loop to iterate over a packing list

---
+++ packing-list.js
@@ -0,0 +1,17 @@
+/* Iterating over a small array 
+
+const packingList = ["tent", "bug spray", "sleeping bag"];
+
+console.log(packingList[0]);
+console.log(packingList[1]);
+console.log(packingList[2]);
+
+*/
+
+//
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+	console.log(packingList[i];
+}
