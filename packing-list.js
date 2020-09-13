f7504c70a9194de79e38381031d6015559209247
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-09-13 03:31:41 +0000

    Using a while loop to iterate over a packing list

--- packing-list.js
+++ packing-list.js
@@ -8,10 +8,24 @@ console.log(packingList[2]);
 
 */
 
-//
+/* Iterating over a larger array using a for loop
+
+
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
-	console.log(packingList[i];
+	console.log(packingList[i]);
+}
+
+*/
+
+// Iterating over an array using a while loop
+
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+let i = 0;
+while (i < packingList.length){
+	console.log(packingList[i]);
+	i++;
 }
